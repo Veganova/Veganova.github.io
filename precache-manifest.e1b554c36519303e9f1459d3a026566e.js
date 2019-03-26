714ac8a0db176de9ccf12ee4037ea9640a209893
Veganova <virajnpatila@gmail.com>
2019-03-25 21:46:45 -0400

    My first website

---
+++ precache-manifest.e1b554c36519303e9f1459d3a026566e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a7bcf3c1eb7e01e00b68fb984bc2f2f",
+    "url": "/static/media/gothic.9a7bcf3c.eot"
+  },
+  {
+    "revision": "34cc487b3cb7d06dc0233644b2fd40da",
+    "url": "/static/media/gothic.34cc487b.ttf"
+  },
+  {
+    "revision": "151c4696847a325b6f5a672ec0c426bc",
+    "url": "/static/media/gothic.151c4696.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "38ee8eeb13c951a152e3",
+    "url": "/static/js/main.38ee8eeb.chunk.js"
+  },
+  {
+    "revision": "42dae3ba417a4a50101f",
+    "url": "/static/js/2.42dae3ba.chunk.js"
+  },
+  {
+    "revision": "38ee8eeb13c951a152e3",
+    "url": "/static/css/main.fd7445a8.chunk.css"
+  },
+  {
+    "revision": "588a6a0cc4f3608668ff9136211eb090",
+    "url": "/index.html"
+  }
+];
