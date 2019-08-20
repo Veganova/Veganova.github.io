f5d1a642133c37aa311356132547d2fcb235a60f
Veganova <virajnpatil@gmail.com>
2019-08-20 13:17:49 -0400

    manual move of build folder and push

---
+++ precache-manifest.4ddda75ff506f785b2d68deefdf03845.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a7bcf3c1eb7e01e00b68fb984bc2f2f",
+    "url": "/static/media/gothic.9a7bcf3c.eot"
+  },
+  {
+    "revision": "34cc487b3cb7d06dc0233644b2fd40da",
+    "url": "/static/media/gothic.34cc487b.ttf"
+  },
+  {
+    "revision": "151c4696847a325b6f5a672ec0c426bc",
+    "url": "/static/media/gothic.151c4696.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c6ff3bc85cc10e661d06",
+    "url": "/static/js/main.c6ff3bc8.chunk.js"
+  },
+  {
+    "revision": "6781a5473c419797ec1b",
+    "url": "/static/js/2.6781a547.chunk.js"
+  },
+  {
+    "revision": "c6ff3bc85cc10e661d06",
+    "url": "/static/css/main.7c2cd105.chunk.css"
+  },
+  {
+    "revision": "e0f1ca646551af1027646a5b748f7ef4",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.96d710c2d5dc2dc5b361c6906c543a26.js"
+  "/precache-manifest.4ddda75ff506f785b2d68deefdf03845.js"
 );
 
 workbox.clientsClaim();
